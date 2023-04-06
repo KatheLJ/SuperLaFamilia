b17499db98a799a492f22fec44a822b206c74e42
KatheLJ <katheloajar257@gmail.com>
2023-04-06 14:12:54 -0600

    UI

--- SuperLaFamilia/Controllers/HomeController.cs
+++ SuperLaFamilia/Controllers/HomeController.cs
@@ -15,16 +15,21 @@ namespace SuperLaFamilia.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            
             return View();
         }
+
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
+
     }
 }
